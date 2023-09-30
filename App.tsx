8d41d6b7d2c0e6b4ec7ff91cbcbd91e9dffe9a08
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2023-09-30 23:20:11 +0500

    Login Signup Start

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+function App(): JSX.Element {
+  
+
+  return (
+    <></>
+  );
+}
+
+
+export default App;
